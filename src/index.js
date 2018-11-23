e247bbbbf2187ada9d19658c3e87a8a2c166003e
thedocbwarren <jedi.master@augmentedjs.com>
2018-11-22 21:20:41 -0800

    exposing storage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import LocalForageCollection from "./localForageCollection.js";
 import LocalForageModel from "./localforageModel.js";
+import LocalForage from "./localforage.js"
 
 module.exports.LocalForageCollection = LocalForageCollection;
 module.exports.LocalForageModel = LocalForageModel;
+module.exports.LocalForage = LocalForage;
